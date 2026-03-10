d09057d4044b76d533e8ecdf8a82089ac3764414
Enzo <enzoniels88@gmail.com>
2026-03-10 10:52:04 +0100

    Ajout de la classe ProgrammeLineaire avec ses méthodes et initialisation.

---
+++ programme_lineaire.py
@@ -0,0 +1,37 @@
+"""..."""
+
+from vecteur import Vecteur
+from matrice import Matrice
+from application_lineaire import ApplicationLineaire
+from systeme_lineaire import SystemeLineaire
+
+class ProgrammeLineaire:
+    
+    def __init__(self, coefficient, matrice, vecteur):
+        """..."""
+        pass
+    
+    def __str__(self):
+        """..."""
+        
+    def __repr__(self):
+        """..."""
+        
+    def construire_tableau(self):
+        """..."""
+        
+    def choisir_pivot(self):
+        """..."""
+        
+    def pivoter(self, lig, col):
+        """..."""
+        
+    def resoudre(self):
+        """..."""
+        
+    def solution(self):
+        """..."""
+    
+    
+    if __name__ == "__main__":
+        pass
